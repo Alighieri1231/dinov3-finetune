b8b10d78bc2ebea035e0c205e36058480d28074c
Rob van Gastel <RobvanGastel@users.noreply.github.com>
2025-09-25 22:09:18 +0200

    Fix bug in the IoU metric from issue #9

--- dino_finetune/metrics.py
+++ dino_finetune/metrics.py
@@ -6,7 +6,7 @@ import torch
 def compute_iou_metric(
     y_hat: torch.Tensor,
     y: torch.Tensor,
-    ignore_index: Optional[int | None] = None,
+    ignore_index: Optional[int] = None,
     eps: float = 1e-6,
 ) -> float:
     """Compute the Intersection over Union metric for the predictions and labels.
@@ -23,18 +23,24 @@ def compute_iou_metric(
     Returns:
         float: The mean IoU
     """
-
+    num_classes = int(y.max().item() + 1)
     y_hat = torch.argmax(y_hat, dim=1)
-    y_hat = y_hat.int()
-    y = y.int()
+    
+    ious = []
+    for c in range(num_classes):
+        y_hat_c = (y_hat == c)
+        y_c = (y == c)
 
-    if ignore_index is not None:
-        mask = y != ignore_index
-        y_hat = y_hat * mask
-        y = y * mask
+        # Ignore all regions with ignore
+        if ignore_index is not None:
+            mask = (y != ignore_index)
+            y_hat_c = y_hat_c & mask
+            y_c = y_c & mask
 
-    intersection = (y_hat & y).float().sum((1, 2))
-    union = (y_hat | y).float().sum((1, 2))
+        intersection = (y_hat_c & y_c).sum().float()
+        union = (y_hat_c | y_c).sum().float()
 
-    iou = (intersection + eps) / (union + eps)
-    return iou.mean()
+        if union > 0:
+            ious.append((intersection + eps) / (union + eps))
+
+    return torch.mean(torch.stack(ious)).item()
